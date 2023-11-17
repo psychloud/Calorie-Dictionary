8b415cfd17f9f14d04649043570fa9e80475ac22
psychloud <146914911+psychloud@users.noreply.github.com>
2023-11-17 17:52:49 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,122 @@
+
+<?php
+require_once("kaloricounter.php")
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div id="judul">
+        <h1>Calory Counter</h1>
+    </div>
+<div id="all">
+        <div id="button">
+            <button><?php $ayamgoreng->getNama(); ?></button>
+            <button><?php $ayamrebus->getNama(); ?></button>
+            <button><?php $ayambakar->getNama(); ?></button>
+            <button><?php $sateayam->getNama(); ?></button>
+            <button><?php $sotoayam->getNama(); ?></button>
+            <button><?php $sotosapi->getNama(); ?></button>
+            <button><?php $baksosapi->getNama(); ?></button>
+            <button><?php $baksoayam->getNama(); ?></button>
+            <button><?php $steak->getNama(); ?></button>
+            <button><?php $rendang->getNama(); ?></button>
+            <button><?php $gulaisapi->getNama(); ?></button>
+            <button><?php $nasiputih->getNama(); ?></button>
+            <button><?php $nasigoreng->getNama(); ?></button>
+            <button><?php $telurgoreng->getNama(); ?></button>
+            <button><?php $telurrebus->getNama(); ?></button>
+            <button><?php $tempegoreng->getNama(); ?></button>
+            <button><?php $temperebus->getNama(); ?></button>
+            <button><?php $tahugoreng->getNama(); ?></button>
+            <button><?php $tahurebus->getNama(); ?></button>
+            <button><?php $beefpizza->getNama(); ?></button>
+            <button><?php $burger->getNama(); ?></button>
+        </div>
+    <div id="kaloriarea">
+    <h2 id="kalorilabel">Kalori</h2>
+    <div id="kalorivalue"></div>
+    </div>
+</div>
+
+    
+
+    <script>
+        const button= document.querySelectorAll("button");
+        const kaloriArea = document.getElementById("kalorivalue");
+        button[0].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $ayamgoreng -> getKalori()?>;
+        })
+        button[1].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $ayamrebus -> getKalori()?>;
+        })
+        button[2].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $ayambakar -> getKalori()?>;
+        })
+        button[3].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $sateayam -> getKalori()?>;
+        })
+        button[4].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $sotoayam -> getKalori()?>;
+        })
+        button[5].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $sotosapi -> getKalori()?>;
+        })
+        button[6].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $baksosapi -> getKalori()?>;
+        })
+        button[7].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $baksoayam -> getKalori()?>;
+        })
+        button[8].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $steak -> getKalori()?>;
+        })
+        button[9].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $rendang -> getKalori()?>;
+        })
+        button[10].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $gulaisapi -> getKalori()?>;
+        })
+        button[11].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $nasiputih -> getKalori()?>;
+        })
+        button[12].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $nasigoreng -> getKalori()?>;
+        })
+        button[13].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $telurgoreng -> getKalori()?>;
+        })
+        button[14].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $telurrebus -> getKalori()?>;
+        })
+        button[15].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $tempegoreng -> getKalori()?>;
+        })
+        button[16].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $temperebus -> getKalori()?>;
+        })
+        button[17].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $tahugoreng -> getKalori()?>;
+        })
+        button[18].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $tahurebus -> getKalori()?>;
+        })
+        button[19].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $beefpizza -> getKalori()?>;
+        })
+        button[20].addEventListener("click",function() {
+            kaloriArea.innerHTML = <?php $burger-> getKalori()?>;
+        })
+    </script>
+</body>
+</html>
---
+++ kaloricounter.php
@@ -0,0 +1,66 @@
+<?php
+    abstract class Makanan{
+        private $namaMakanan;
+        private $kalori;
+        
+        public function getNama(){
+            echo $this->namaMakanan;
+        }
+        public function getKalori(){
+            echo $this->kalori;
+        }
+
+        public function __construct($namaMakanan, $kalori){
+            $this->namaMakanan = $namaMakanan;
+            $this->kalori = $kalori;
+        }
+    }
+
+
+class Ayam extends Makanan{
+
+    }  
+    class Tempe extends Makanan{
+
+    }
+
+    class Tahu extends Makanan{
+
+    }
+
+    class Nasi extends Makanan{
+        
+    }
+
+    class Telur extends Makanan{
+        
+    }
+
+    class Daging extends Makanan{
+        
+    }
+
+
+    $ayamgoreng = new Ayam("Ayam Goreng", 245.9);
+    $ayamrebus = new Ayam("Ayam rebus", 150);
+    $ayambakar = new Ayam("Ayam bakar", 216);
+    $sateayam = new Ayam("Sate ayam", 466);
+    $sotoayam = new Ayam("Soto Ayam", 130);
+    $sotosapi = new Daging("Soto sapi", 91);
+    $baksosapi = new Daging("Bakso sapi", 202);
+    $baksoayam = new Ayam("Bakso ayam", 161);
+    $steak = new Daging("Steak sapi", 252);
+    $rendang = new Daging("Rendang", 195);
+    $gulaisapi = new Daging("Gulai sapi", 109);
+    $nasiputih = new Nasi("Nasi putih", 129);
+    $nasigoreng = new Nasi("Nasi goreng", 168);
+    $telurgoreng = new Telur("Telur goreng", 154);
+    $telurrebus = new Telur("Telur rebus", 154);
+    $tempegoreng = new Tempe("Tempe goreng", 255);
+    $temperebus = new Tempe("Tempe goreng", 196);
+    $tahugoreng = new Tahu("Tahu goreng", 271);
+    $tahurebus = new Tahu("Tahu rebus", 154);
+    $beefpizza = new Daging("Beef pizza", 301);
+    $burger = new Daging("Burger", 258);
+
+?>
